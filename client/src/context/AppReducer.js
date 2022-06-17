2cc2dd91771cc1966e5eea37edd9b9971a40a283
Akshat Toolaj Sinha <2020csb1068@iitrpr.ac.in>
2022-06-18 01:57:29 +0530

    retailer_adding_corner_cases

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,10 +1,26 @@
 export default(state,action)=>{
     switch(action.type){
         case 'UPDATE_RETAILER_LIST':{
+            const newretailerList=[];
+            let togg=0;
+            state.retailerList.map( (element)=>{
+
+                if( element.Item===action.payload.Item){
+                    const newquantity=parseInt(element.Quantity)+parseInt(action.payload.Quantity);
+                    if(newquantity>0)
+                        newretailerList.push( {"Item":element.Item,"Quantity":newquantity});
+                    togg=1;
+                }
+                else{
+                    newretailerList.push(element);
+                }
+            });
+            if(!togg && parseInt(action.payload.Quantity)>0)
+                newretailerList.push(action.payload);
 
             return{
                 ...state,
-                retailerList:[...state.retailerList,action.payload]
+                retailerList:newretailerList
             }
         }
         default:
